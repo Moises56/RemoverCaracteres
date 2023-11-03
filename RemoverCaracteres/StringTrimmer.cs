646211b35387492c9ab02a7e1ec5c3a681063d40
Moises Aviles <moiavil56@gmail.com>
2023-11-03 10:41:48 -0600

    first commit

---
+++ RemoverCaracteres/StringTrimmer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RemoverCaracteres
+{
+    public class StringTrimmer
+    {
+        public static string RemoveSpaces(string input)
+        {
+            return input.Replace(" ", string.Empty);
+        }
+        /*public static string TrimSequence(string inputString, string sequence = " ")
+        {
+            return inputString.Replace(sequence, "");
+        }*/
+    }
+}
